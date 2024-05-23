1750e4862ba8bd3654410a485e4e2665b47e8d8f
Yusuf <yusufmohd72@gmail.com>
2024-05-24 00:54:56 +0530

    optimize async code

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -32,127 +32,141 @@ const Page = async () => {
 
   console.log(userId);
 
-  const mongoUser = await getUserById({ userId });
+  try {
+    const mongoUser = await getUserById({ userId });
 
-  const result = await getUserTransactions({ userId: mongoUser, limit: 5 });
-  const totalIncome = await getOverallIncome({ userId: mongoUser });
-  const totalExpenses = await getOverallExpenses({ userId: mongoUser });
-  const totalBalance = await getOverallBalance({ userId: mongoUser });
+    // Run these operations in parallel
+    const [result, totalIncome, totalExpenses, totalBalance, cycle] =
+      await Promise.all([
+        getUserTransactions({ userId: mongoUser, limit: 5 }),
+        getOverallIncome({ userId: mongoUser }),
+        getOverallExpenses({ userId: mongoUser }),
+        getOverallBalance({ userId: mongoUser }),
+        getCurrentCycle({ userId: mongoUser }),
+      ]);
 
-  const cycle = (await getCurrentCycle({ userId: mongoUser })) || null;
+    // If cycle exists, run these operations in parallel
+    const [cycleExpenses, cycleBalance] = cycle
+      ? await Promise.all([
+          getCycleTotalExpenses({ cycleId: cycle._id }),
+          getRemainingCycleBalance({ cycleId: cycle._id }),
+        ])
+      : [null, null];
 
-  const cycleExpenses = await getCycleTotalExpenses({ cycleId: cycle?._id });
-  const cycleBalance = await getRemainingCycleBalance({ cycleId: cycle?._id });
+    return (
+      <>
+        <section className="w-full max-sm:px-2 px-6">
+          <div className="md:flex items-center justify-between font-semibold mb-12 text-xl">
+            <p className="max-sm:mb-4">It&apos;s {date}</p>
+            <div className="sm:flex gap-4">
+              <Link href="/add-transaction">
+                <Button className="p-6 primary-gradient w-full sm:w-auto mb-2 sm:mb-0">
+                  Add Transaction
+                </Button>
+              </Link>
 
-  return (
-    <>
-      <section className="w-full max-sm:px-2 px-6">
-        <div className="md:flex items-center justify-between font-semibold mb-12 text-xl">
-          <p className="max-sm:mb-4">It&apos;s {date}</p>
-          <div className="sm:flex gap-4">
-            <Link href="/add-transaction">
-              <Button className="p-6 primary-gradient w-full sm:w-auto mb-2 sm:mb-0">
-                Add Transaction
-              </Button>
-            </Link>
-
-            <CycleSheet currentCycle={cycle} />
-          </div>
-        </div>
-
-        <div className="md:flex gap-6">
-          <div className="md:w-2/3">
-            <div className="mb-6">
-              <CycleOverview
-                mongoUser={mongoUser}
-                cycle={cycle}
-                cycleExpenses={cycleExpenses}
-                cycleBalance={cycleBalance}
-              />
+              <CycleSheet currentCycle={cycle} />
             </div>
+          </div>
 
-            <div className="mb-6">
-              <h2 className="text-xl font-semibold mb-6">Overall</h2>
-              <div className="md:flex items-center justify-between gap-6 max-sm:space-y-3">
-                <div className="p-8 flex-1 rounded-md border border-1 ">
-                  <div className="flex items-center justify-between mb-4">
-                    <p>Income</p>
-                    <TrendingUp size={20} color="green" />
-                  </div>
-                  <p className=" text-2xl font-bold">
-                    ₹ {formatNumberWithCommas(totalIncome)}
-                  </p>
-                </div>
+          <div className="md:flex gap-6">
+            <div className="md:w-2/3">
+              <div className="mb-6">
+                <CycleOverview
+                  mongoUser={mongoUser}
+                  cycle={cycle}
+                  cycleExpenses={cycleExpenses}
+                  cycleBalance={cycleBalance}
+                />
+              </div>
 
-                {/* large screens */}
-                <div className="p-8 flex-1 rounded-md border border-1 max-sm:hidden">
-                  <div className="flex items-center justify-between mb-4">
-                    <p>Expenses</p>
-                    <TrendingDown size={20} color="red" />
-                  </div>
-                  <p className="text-2xl font-bold">
-                    ₹ {formatNumberWithCommas(totalExpenses)}
-                  </p>
-                </div>
-                <div className="p-8 flex-1 rounded-md border border-1 max-sm:hidden">
-                  <div className="flex items-center justify-between mb-4">
-                    <p>Balance</p>
-                    <PiggyBank size={20} color="blue" />
-                  </div>
-                  <p className="text-2xl font-bold">
-                    ₹ {formatNumberWithCommas(totalBalance)}
-                  </p>
-                </div>
-
-                {/* small screens */}
-                <div className="max-sm:flex max-sm:gap-4 hidden">
+              <div className="mb-6">
+                <h2 className="text-xl font-semibold mb-6">Overall</h2>
+                <div className="md:flex items-center justify-between gap-6 max-sm:space-y-3">
                   <div className="p-8 flex-1 rounded-md border border-1 ">
+                    <div className="flex items-center justify-between mb-4">
+                      <p>Income</p>
+                      <TrendingUp size={20} color="green" />
+                    </div>
+                    <p className=" text-2xl font-bold">
+                      ₹ {formatNumberWithCommas(totalIncome)}
+                    </p>
+                  </div>
+
+                  {/* large screens */}
+                  <div className="p-8 flex-1 rounded-md border border-1 max-sm:hidden">
                     <div className="flex items-center justify-between mb-4">
                       <p>Expenses</p>
                       <TrendingDown size={20} color="red" />
                     </div>
-                    <p className="max-sm:text-lg text-2xl font-bold">
+                    <p className="text-2xl font-bold">
                       ₹ {formatNumberWithCommas(totalExpenses)}
                     </p>
                   </div>
-                  <div className="p-8 flex-1 rounded-md border border-1 ">
+                  <div className="p-8 flex-1 rounded-md border border-1 max-sm:hidden">
                     <div className="flex items-center justify-between mb-4">
                       <p>Balance</p>
                       <PiggyBank size={20} color="blue" />
                     </div>
-                    <p className="max-sm:text-lg text-2xl font-bold">
+                    <p className="text-2xl font-bold">
                       ₹ {formatNumberWithCommas(totalBalance)}
                     </p>
+                  </div>
+
+                  {/* small screens */}
+                  <div className="max-sm:flex max-sm:gap-4 hidden">
+                    <div className="p-8 flex-1 rounded-md border border-1 ">
+                      <div className="flex items-center justify-between mb-4">
+                        <p>Expenses</p>
+                        <TrendingDown size={20} color="red" />
+                      </div>
+                      <p className="max-sm:text-lg text-2xl font-bold">
+                        ₹ {formatNumberWithCommas(totalExpenses)}
+                      </p>
+                    </div>
+                    <div className="p-8 flex-1 rounded-md border border-1 ">
+                      <div className="flex items-center justify-between mb-4">
+                        <p>Balance</p>
+                        <PiggyBank size={20} color="blue" />
+                      </div>
+                      <p className="max-sm:text-lg text-2xl font-bold">
+                        ₹ {formatNumberWithCommas(totalBalance)}
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <div className="md:w-1/3">
-            <h2 className="text-xl font-semibold mb-6">Recent transactions</h2>
-            {result!.transactions.length > 0 ? (
-              result!.transactions.map((item: any) => (
-                <div className="flex items-center justify-between text-lg p-4 border mb-3 rounded-md">
-                  <div className="flex gap-2">
-                    {item.transactionType === "income" ? (
-                      <TrendingUp />
-                    ) : (
-                      <TrendingDown />
-                    )}
-                    <p className="max-w-[120px] line-clamp-1"> {item.name}</p>
+            <div className="md:w-1/3">
+              <h2 className="text-xl font-semibold mb-6">
+                Recent transactions
+              </h2>
+              {result!.transactions.length > 0 ? (
+                result!.transactions.map((item: any) => (
+                  <div className="flex items-center justify-between text-lg p-4 border mb-3 rounded-md">
+                    <div className="flex gap-2">
+                      {item.transactionType === "income" ? (
+                        <TrendingUp />
+                      ) : (
+                        <TrendingDown />
+                      )}
+                      <p className="max-w-[120px] line-clamp-1"> {item.name}</p>
+                    </div>
+                    <p>₹ {formatNumberWithCommas(item.amount)}</p>
                   </div>
-                  <p>₹ {formatNumberWithCommas(item.amount)}</p>
-                </div>
-              ))
-            ) : (
-              <p>No recent transactions 😐. Go add some!</p>
-            )}
+                ))
+              ) : (
+                <p>No recent transactions 😐. Go add some!</p>
+              )}
+            </div>
           </div>
-        </div>
-      </section>
-    </>
-  );
+        </section>
+      </>
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 };
 
 export default Page;
